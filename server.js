4bbed0dc8e5e4847661e9de3cf8f73eee6b6e4c1
Alexandros Mentoudakis <a.mendoudakis@gmail.com>
2025-02-14 17:56:00 +0200

    small changes

--- server.js
+++ server.js
@@ -4,18 +4,6 @@ const app = require("./app");
 const { initMongoDB } = require("./services/db");
 
 // Initialize MongoDB, but DO NOT block the export
-initMongoDB()
-	.then((isDBInitialized) => {
-		if (!isDBInitialized) {
-			console.error("MongoDB initialization failed. Exiting...");
-			process.exit(1);
-		} else {
-			console.log("✅ MongoDB Connected Successfully");
-		}
-	})
-	.catch((err) => {
-		console.error("Error connecting to MongoDB:", err);
-		process.exit(1);
-	});
+initMongoDB();
 
-module.exports = app;
+app.listen(8080, () => {});
