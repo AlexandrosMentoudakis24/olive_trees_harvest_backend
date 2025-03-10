16634c0d2d5109e350807f9edeb232d6892d9358
Alexandros Mentoudakis <a.mendoudakis@gmail.com>
2025-03-10 18:08:00 +0200

    latest commit

--- app.js
+++ app.js
@@ -3,37 +3,21 @@ const express = require("express");
 
 const expressApp = express();
 
-const authRoutes = require("./routes/auth/auth");
+const corsConfiguration = require("./middlewares/cors_configuration");
 
 const oliveTreesHarvestRoutes = require("./routes/olive_trees_harvest/olive_trees_harvest");
-
 const oliveTreesExpensesRoutes = require("./routes/olive_trees_harvest/olive_trees_expense");
 const oliveMillingsRoutes = require("./routes/olive_trees_harvest/olive_milling");
 const oliveSellingsRoutes = require("./routes/olive_trees_harvest/olive_selling");
-
-const corsConfiguration = require("./middlewares/cors_configuration");
-
-const { User } = require("./models/user");
+const authRoutes = require("./routes/auth/auth");
 
 expressApp.use(bodyParser.json({ limit: "250mb" }));
 expressApp.use(bodyParser.urlencoded({ extended: true }));
-
 expressApp.use(corsConfiguration);
 
-expressApp.get("/hello", async (req, res, next) => {
-	console.log("Test was successful.");
-
-	const users = await User.find();
-
-	res.status(200).send({ message: "Test Request worked.", user: users[0] });
-});
-
 expressApp.use("/auth", authRoutes);
-
 expressApp.use("/oliveTreesHarvests", oliveTreesHarvestRoutes);
-
 expressApp.use("/oliveTreesExpenses", oliveTreesExpensesRoutes);
-
 expressApp.use("/oliveMillings", oliveMillingsRoutes);
 expressApp.use("/oliveSellings", oliveSellingsRoutes);
 
--- services/db.js
+++ services/db.js
@@ -1,27 +1,27 @@
 const mongoose = require("mongoose");
 
 exports.initMongoDB = async () => {
-  const mongoDBConnectionUri = process.env.MONGO_DB_CONNECTION_URI;
-  const mongoDBName = process.env.MONGO_DB_NAME;
+	const mongoDBConnectionUri = process.env.MONGO_DB_CONNECTION_URI;
+	const mongoDBName = process.env.MONGO_DB_NAME;
 
-  const mongoConnectionUri = mongoDBConnectionUri + mongoDBName;
+	const mongoConnectionUri = mongoDBConnectionUri + mongoDBName;
 
-  try {
-    if (
-      mongoConnectionUri == undefined ||
-      mongoDBConnectionUri == undefined ||
-      mongoDBName == undefined
-    ) {
-      throw new Error("Mongo URI variable is not defined!");
-    }
+	try {
+		if (
+			mongoConnectionUri == undefined ||
+			mongoDBConnectionUri == undefined ||
+			mongoDBName == undefined
+		) {
+			throw new Error("Mongo URI variable is not defined!");
+		}
 
-    await mongoose.connect(mongoConnectionUri, {});
-    console.log("Mongo DB is connected!");
+		await mongoose.connect(mongoConnectionUri, {});
+		console.log("Mongo DB is connected!");
 
-    return true;
-  } catch (err) {
-    console.log("Mongo DB connection failed!");
+		return true;
+	} catch (err) {
+		console.log("Mongo DB connection failed!");
 
-    return true;
-  }
+		return true;
+	}
 };
